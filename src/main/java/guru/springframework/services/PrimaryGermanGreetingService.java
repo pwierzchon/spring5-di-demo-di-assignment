72945f70c4b4b34251ab24c1e7d1f5e910d5a58c
pwierzchon <destino.fordon@gmail.com>
2017-08-26 11:13:47 +0200

    Fixed code - GreetingRepositoryImpl didn't have @Service annotation so it couldn't be detected as a bean. PromaryGermanGreetingService didnt' have any DI for GreetingRepository.

--- src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
+++ src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
@@ -1,5 +1,6 @@
 package guru.springframework.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,10 @@ public class PrimaryGermanGreetingService implements GreetingService {
 
     private GreetingRepository greetingRepository;
 
+    public PrimaryGermanGreetingService(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
+    
     @Override
     public String sayGreeting() {
         return greetingRepository.getGermanGreeting();
